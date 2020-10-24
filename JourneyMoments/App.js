fb2b78ba23f252fd634677df70dd1fea40680a86
Enar Mariinsky <enar.mariinsky@gmail.com>
2020-10-24 21:05:10 +0300

    added react-native-permissions lib

--- JourneyMoments/App.js
+++ JourneyMoments/App.js
@@ -24,6 +24,8 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+import {PERMISSIONS} from 'react-native-permissions';
+
 const App: () => React$Node = () => {
   return (
     <>
