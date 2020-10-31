3ec374c92ff8edf0ac1ad04bdb46fb09f894b9fb
Enar Mariinsky <enar.mariinsky@gmail.com>
2020-10-31 16:59:32 +0200

    icons?

--- JourneyMoments/navigation/TabNavigator.js
+++ JourneyMoments/navigation/TabNavigator.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Icon from 'react-native-ionicons'
 import Home from '../screens/Home'
 import Trips from '../screens/Trips'
 import Stats from '../screens/Stats'
@@ -8,7 +9,28 @@ import Stats from '../screens/Stats'
 const Tab = createBottomTabNavigator()
 const TabNavigator = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName
+
+          if (route.name === 'Home') {
+            iconName = focused
+              ? 'home'
+              : 'home'
+          } else if (route.name === 'Settings') {
+            iconName = focused ? 'ios-list-box' : 'ios-list'
+          }
+
+          // You can return any component that you like here!
+          return <Icon name={iconName} size={size} color={color} />
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: 'tomato',
+        inactiveTintColor: 'gray',
+      }}
+    >
       <Tab.Screen name="Home" component={Home} />
       <Tab.Screen name="Trips" component={Trips} />
       <Tab.Screen name="Stats" component={Stats} />
--- JourneyMoments/screens/Home.js
+++ JourneyMoments/screens/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import Icon from 'react-native-ionicons'
 
 const Home = () => {
   return (
     <View>
       <Text>Home</Text>
+      <Icon name={'home'} />
     </View>
   )
 }
