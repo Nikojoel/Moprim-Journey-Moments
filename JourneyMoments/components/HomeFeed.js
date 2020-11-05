c01954c8c673fcde5dd7d6cce62add5083a7a5ed
Enar Mariinsky <enar.mariinsky@gmail.com>
2020-11-05 12:58:01 +0200

    commented trips to home (#25)

---
+++ JourneyMoments/components/HomeFeed.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { FlatList, SafeAreaView, StatusBar } from "react-native";
+import { Text, ListItem, Left, Body, Icon, Right, Title } from "native-base"
+import HomeFeedItem from "./HomeFeedItem";
+
+
+const HomeFeed = ({data, extra}) => {
+
+    return (
+        <SafeAreaView>
+            <FlatList
+                data={data}
+                renderItem={HomeFeedItem}
+                keyExtractor={item => item.id}
+                extraData={extra}
+            />
+        </SafeAreaView>
+    )
+}
+
+export default HomeFeed
